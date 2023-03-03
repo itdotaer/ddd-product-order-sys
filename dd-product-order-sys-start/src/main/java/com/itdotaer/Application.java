440c8e54a10cff39421e74f9f80e1215b3e6d143
JiangtaoHu <jiangtao.hu@shopee.com>
2023-03-03 18:48:03 +0800

    支持sharding sphere分库分表

--- dd-product-order-sys-start/src/main/java/com/itdotaer/Application.java
+++ dd-product-order-sys-start/src/main/java/com/itdotaer/Application.java
@@ -1,13 +1,9 @@
 package com.itdotaer;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * Spring Boot Starter
